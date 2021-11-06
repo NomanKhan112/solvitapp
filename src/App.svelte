db3a60d4509a34f1c851872ab8e03f50494129bc
Nouman Khan <bigherohnz@gmail.com>
2021-11-06 05:06:37 -0700

    Noumman change

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,35 @@
 <script>
-	let name = 'world';
+	let name = 'BTC';
+	let value ='600$';
 </script>
-
-<h1>Hello {name}!</h1>
 <style>
 	th {
 		color: rgb(196, 58, 122);
 		font-family: 'Comic Sans MS', cursive;
 		font-size: 1em;
 	}
-	/* table {
-		border-spacing: 10px;
-		width: 60%;
-		dfsdfasdfasfasdfa
-		height: 50%;
-		border-radius: 8px;
-		border: 2px solid rgb(106, 66, 143);
-		margin: 80px 40px;
-		display: flex;
-		justify-content:space-evenly;
-		align-items: center;
-	} */
 	td {
 		white-space: normal;
 		padding: 17px;
 		word-spacing: normal;
 		font-size: 1rem;
 	}
+	
+	.contaner{
+		border-radius: 10px ;
+		border:none;
+		margin: 0px 200px;
+		background-color: #DCEEFE;
+		align-items: center;
+		height: 100px;
+	}
 </style>
 
 <section>
-	<div>
-		<table>
+	<div >
+		<table class="contaner">
 			<tr>
-			  <th>BTC</th>
+			  <th>{name}</th>
 			  <th>ETH</th>
 			  <th>ADA</th>
 			  <th>BNB</th>
@@ -43,7 +39,7 @@
 			  <th>Dogecoin</th>
 			</tr>
 			<tr>
-			  <td>6000$</td>
+			  <td>{value}</td>
 			  <td>4000$</td>
 			  <td>10$</td>
 			  <td>100$</td>
