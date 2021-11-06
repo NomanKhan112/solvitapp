6011cff0b9d0735d1318cc2785ea00be53b7e7d4
Nouman Khan <bigherohnz@gmail.com>
2021-11-06 04:28:04 -0700

    solvit app

---
+++ src/App.svelte
@@ -0,0 +1,56 @@
+<script>
+	let name = 'world';
+</script>
+
+<h1>Hello {name}!</h1>
+<style>
+	th {
+		color: rgb(196, 58, 122);
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 1em;
+	}
+	/* table {
+		border-spacing: 10px;
+		width: 60%;
+		height: 50%;
+		border-radius: 8px;
+		border: 2px solid rgb(106, 66, 143);
+		margin: 80px 40px;
+		display: flex;
+		justify-content:space-evenly;
+		align-items: center;
+	} */
+	td {
+		white-space: normal;
+		padding: 17px;
+		word-spacing: normal;
+		font-size: 1rem;
+	}
+</style>
+
+<section>
+	<div>
+		<table>
+			<tr>
+			  <th>BTC</th>
+			  <th>ETH</th>
+			  <th>ADA</th>
+			  <th>BNB</th>
+			  <th>Tether</th>
+			  <th>Solana</th>
+			  <th>XRP</th>
+			  <th>Dogecoin</th>
+			</tr>
+			<tr>
+			  <td>6000$</td>
+			  <td>4000$</td>
+			  <td>10$</td>
+			  <td>100$</td>
+			  <td>20$</td>
+			  <td>60$</td>
+			  <td>80$</td>
+			  <td>110$</td>
+			</tr>
+		  </table>
+	</div>
+</section>
